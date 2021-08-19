b03d3a3e7d3ee6b0db6e2745f6498c2d4e7ab3db
vnishant24 <84161849+vnishant24@users.noreply.github.com>
2021-08-18 21:27:58 -0700

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { FaAlignRight } from 'react-icons/fa';
+import './App.css';
+
+export default class App extends Component {
+    state = {
+        toggle:false
+    }
+    Toggle = () => {
+        this.setState({toggle:!this.state.toggle})
+    }
+    render() {
+        return (
+          <>
+              <div className="navBar">
+                    <button onClick={this.Toggle}>
+                        <FaAlignRight />
+                    </button>
+                    <ul className={this.state.toggle ? "nav-links show-nav" : "nav-links"}>
+                        <li href="#">Home</li>
+                        <li href="#">About us</li>
+                        <li href="#">Contact</li>
+                    </ul>
+              </div>
+          </>
+        );
+    }
+}
